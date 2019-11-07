503a1486c6e41d7466542fe3e1ea83303e026467
ferchuse <sistemas@glifo.mx>
2019-11-07 12:38:31 -0600

    Formulario

---
+++ scripts.php
@@ -0,0 +1,26 @@
+
+<!-- jQuery library -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+
+<script src="../../vendor/jquery/alertify.min.js"></script>
+
+
+<!-- Popper JS -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
+
+
+<!-- Custom scripts for all pages-->
+<script src="../../js/sb-admin.min.js"></script>
+<script src="../../js/date.js"></script>
+<script src="../../js/notificaciones.js"></script>
+
+<script src="../../lib/metro-4.2.34-715-5c41a2cc28af8.js"></script>
+<script src="../../lib/jquery.formnavigation.js"></script>
+<script src= "../../lib/jquery.autocomplete.min.js"> </script>
+<!--
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.0.5/css/tableexport.min.css" />
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/TableExport/5.0.5/js/tableexport.min.js"></script>
+-->
